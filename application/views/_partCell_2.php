d9a06ce612885f9d7eb02ba180a5e2cd77fb4fa4
laurayang321 <laurayang321@gmail.com>
2017-02-11 16:24:55 -0800

    Change the part view layout using template

--- application/views/_partCell_2.php
+++ application/views/_partCell_2.php
@@ -2,4 +2,4 @@
 <a href="/part/gimme/{partID}">
     <img src="/data/parts/{pic}" title="part {partCode}: The {piece} of {model} model reserved for {line} bots line; ${cost}" />
 </a>
-<input type="checkbox" name="partCheckBox" value="{partID}"  class="checkbox">
+
