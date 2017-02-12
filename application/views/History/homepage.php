114c67ebccc4b35bf24dc26fc618bc5f23990515
laurayang321 <laurayang321@gmail.com>
2017-02-11 23:04:54 -0800

    changed records view layout

--- application/views/History/homepage.php
+++ application/views/History/homepage.php
@@ -9,14 +9,17 @@
 ?>
 
 
-<div class="row">
-    <h2>Assemblies</h2>
-    <table>
-        <tr>
-            <td>Assembly ID</td>
-            <td>Date</td>
-            <td>Robot ID</td>
-        </tr>
+<div class = "container">
+    <h2>Assembled Robots History</h2>
+    <table class = "table table-striped">
+        <thead bgcolor="#f2cdb3">
+            <tr>
+                <td>Assembly ID</td>
+                <td>Date</td>
+                <td>Robot ID</td>
+            </tr>
+        <thead>
+        <tbody>
         {assembly}
         <tr>
             <td>{assemblyID}</td>
@@ -24,17 +27,21 @@
             <td>{robotID}</td>
         </tr>
         {/assembly}
+        </tbody>
     </table>
 </div>
 
-<div class="row">
-    <h2>Shipments</h2>
-    <table>
-        <tr>
-            <td>Shipments ID</td>
-            <td>Date</td>
-            <td>Robot ID</td>
-        </tr>
+<div class = "container">
+    <h2>Shipments of Robots History</h2>
+    <table class = "table table-striped">
+        <thead bgcolor="#f2cdb3">
+            <tr>
+                <td>Shipments ID</td>
+                <td>Date</td>
+                <td>Robot ID</td>
+            </tr>
+        </thead>
+        <tbody>
         {shipment}
         <tr>
             <td>{shipmentID}</td>
@@ -42,42 +49,50 @@
             <td>{robotID}</td>
         </tr>
         {/shipment}
+    </tbody>
     </table>
 </div>
 
-<div class="row">
-    <h2>Purchases</h2>
-    <table>
+<div class = "container">
+    <h2>Purchases of Parts History</h2>
+    <table class = "table table-striped">
+        <thead bgcolor="#f2cdb3">
         <tr>
             <td>Purchase ID</td>
             <td>Date</td>
             <td>Part ID</td>
         </tr>
+        </thead>
+        <tbody>
         {purchase}
         <tr>
             <td>{purchaseID}</td>
             <td>{date}</td>
-            <td>{partID}</td>
+            <td>{partIDs}</td>
         </tr>
         {/purchase}
+        </tbody>
     </table>
 </div>
 
-<div class="row">
-    <h2>Returns</h2>
-    <table>
+<div class = "container">
+    <h2>Returns of Parts History</h2>
+    <table class = "table table-striped">
+        <thead bgcolor="#f2cdb3">
         <tr>
             <td>Return ID</td>
             <td>Date</td>
             <td>Part ID</td>
         </tr>
-        {return}
-        <tr>
-            <td>{returnID}</td>
-            <td>{date}</td>
-            <td>{partID}</td>
-        </tr>
-        {/return}
+        </thead>
+        <tbody>
+            {return}
+            <tr>
+                <td>{returnID}</td>
+                <td>{date}</td>
+                <td>{partID}</td>
+            </tr>
+            {/return}
+        </tbody>
     </table>
 </div>
-
