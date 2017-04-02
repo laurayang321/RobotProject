91182a22d3d822bf65e89d2d377689a2f1860924
laurayang321 <laurayang321@gmail.com>
2017-04-01 19:20:25 -0700

    add foreign keys to history table and change  part controller

---
+++ application/views/_partCell_3.php
@@ -0,0 +1,9 @@
+<div class="row text-center">
+    <div class="thumbnail">    
+      
+      <img src="/data/parts/{pic}" title="part {partName}: The {pieceName} of {model} model reserved for {line} bots line;" />
+ 
+      <p><strong>{partName}</strong></p>
+      <p>{stamp}</p>
+    </div>
+</div>  
